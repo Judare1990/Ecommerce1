b6fc1d30c0d02ab052eceedf38236f660f2d0689
alpachm <alexpacheco407@gmail.com>
2023-01-11 23:08:17 -0500

    layout

--- main.js
+++ main.js
@@ -1,56 +1,51 @@
 let collections = [
-    {
-        id: 'o',
-        Name: 'Hoodies', 
-        Price: 14.00,
-        Stock: 10,
-        urlImage: "./featured1.png" 
-    },
-    {
-        id: '1',
-        Name: 'Shirts', 
-        Price: 24.00,
-        Stock: 15,
-        urlImage: "./featured2.png" 
-    },
-    {
-        id: '2',
-        Name: 'Sweatshirts', 
-        Price: 24.00,
-        Stock: 20,
-        urlImage: "./featured1.png" 
-    }
+  {
+    id: 'o',
+    Name: 'Hoodies',
+    Price: 14.00,
+    Stock: 10,
+    urlImage: "./images/featured1.png"
+  },
+  {
+    id: '1',
+    Name: 'Shirts',
+    Price: 24.00,
+    Stock: 15,
+    urlImage: "./images/featured2.png"
+  },
+  {
+    id: '2',
+    Name: 'Sweatshirts',
+    Price: 24.00,
+    Stock: 20,
+    urlImage: "./images/featured3.png"
+  }
 ]
 
 
 const products = document.querySelector(".products");
 
-function printProducts(){
-    let html= '';
-    collections.forEach(function({id, Name, Price, Stock, urlImage}){
-        html += `
+function printProducts() {
+  let html = '';
+  collections.forEach(function ({ id, Name, Price, Stock, urlImage }) {
+    html += `
+
         <div class="product">
+
         <div class="product__img">
           <img src="${urlImage}" alt="${Name}">
+          <button id= "${id}" class="btn btn__add"><i class='bx bx-plus' ></i></button>
         </div>
 
-        <div class="product__options" id= "${id}">
-          <button class="btn btn__add">+</button>
+        <div class="product__info">        
+          <p><b>Name</b>: ${Name}</p>
+          <p><b>Price</b>: ${Price}</p>
+          <p><b>Stock</b>: ${Stock}</p>
         </div>
-
-        
-      
-        <div class="product__info">
-        
-          <p>Name: ${Name}</p>
-          <p>Price: ${Price}</p>
-          <p>Stock: ${Stock}</p>
-        </div>
-      
-        
+             
       </div>
 `
-    });
-    products.innerHTML = html
+  });
+  products.innerHTML = html
 }
 printProducts()
